7bebb1d68d14d38e6b1baeaf225a4a24c05fb7f9
JasonRUAN <rzexin@gmail.com>
2025-05-07 10:41:25 +0000

    update url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -39,5 +39,5 @@ export const CONSTANTS = {
     // BACKEND_URL: "http://localhost:5050",
     BACKEND_URL:
         "https://7eaccb584f75db4d525e57b8ef7f1bb23581455b-5050.dstack-prod5.phala.network",
-    BACKEND_URL2: "http://192.226.147.251:5050",
+    BACKEND_URL2: "http://129.226.147.251:5050",
 };
